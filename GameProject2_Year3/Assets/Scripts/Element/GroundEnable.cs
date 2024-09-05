0f9008381d5637f91ed51febecddb6dd8b14193d
ThwDog <thanadul.sod.work@gmail.com>
2024-09-05 15:05:11 +0700

    a bit fix on enable obj on cam

--- GameProject2_Year3/Assets/Scripts/Element/GroundEnable.cs
+++ GameProject2_Year3/Assets/Scripts/Element/GroundEnable.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent (typeof(EnableOnCam))]
 public class GroundEnable : MonoBehaviour, MapOpenable
 {
     public enum type
@@ -21,55 +22,33 @@ public class GroundEnable : MonoBehaviour, MapOpenable
     [SerializeField][Range(0, 100)] private float dis = 10; // check distance form player and ground 
     Transform player;
     Collider colliders;
-    // [SerializeField] Mesh mesh;
+    EnableOnCam enableOnCam;
     public bool mapOpen = true;
-    [Header("CamDetect")]
-    Camera cam;
-    MeshRenderer _renderer;
-    Plane[] cameraFrustum;
-
 
     private void Start()
     {
         player = GameObject.FindAnyObjectByType<PlayerController>().GetComponent<Transform>();
-
-        // get camera component from camera name FrustumCam 
-        cam = GameObject.Find("FrustumCam").GetComponent<Camera>();
-        _renderer = GetComponent<MeshRenderer>();
+        enableOnCam = GetComponent<EnableOnCam>();
 
         if (types == type.mesh) colliders = GetComponent<MeshCollider>();
         else if (types == type.box) colliders = GetComponent<BoxCollider>();
-
-        // if (groundType == GroundType.grass)
-        // {
-        //     Collider[] hit = Physics.OverlapSphere(transform.position, 2 ,8);
-            
-        //     if (hit != null)
-        //     {
-        //         GetComponent<MeshFilter>().mesh = mesh;
-        //     }
-        // }
     }
 
     private void Update()
     {
         if (!mapOpen) return;
-        // check if game obj is on cam if not disable it
-        cameraFrustum = GeometryUtility.CalculateFrustumPlanes(cam);
-    
-        if(GeometryUtility.TestPlanesAABB(cameraFrustum,colliders.bounds)){
-            _renderer.enabled = true;
-            // check if player is near floor?
-            if (Vector3.Distance(player.position, transform.position) < dis && !colliders.enabled)
-            {
-                colliders.enabled = true;
-            }
-            else if (Vector3.Distance(player.position, transform.position) > dis && colliders.enabled)
-            {
-                colliders.enabled = false;
-            }
+
+        if(!enableOnCam._isEnable) return;
+
+        if (Vector3.Distance(player.position, transform.position) < dis && !colliders.enabled)
+        {
+            colliders.enabled = true;
         }
-        else _renderer.enabled = false;
+        else if (Vector3.Distance(player.position, transform.position) > dis && colliders.enabled)
+        {
+            colliders.enabled = false;
+        }
+
     }
 
     public void Open()
